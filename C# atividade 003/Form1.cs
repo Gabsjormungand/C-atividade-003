c43644019f48cb01bcbfd684c2521d7088b4f983
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-05-15 21:14:37 -0300

    Adicionar arquivos de projeto.

---
+++ C# atividade 003/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__atividade_003
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            int valor1 = Convert.ToInt32(textBox1.Text);
+            int valor2 = Convert.ToInt32(textBox2.Text);
+            int valor3 = Convert.ToInt32(textBox3.Text);
+
+            double Media = (valor1 + valor2 + valor3) / 3;
+            MessageBox.Show($"A media das idade é: {Media}", "Média das idades");
+        }
+    }
+}
